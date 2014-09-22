8d7e77cf77a42c0800d33f2e463f734a2a3295f1
Max Goncharov <mxgoncahrov@gmail.com>
2014-09-22 16:58:30 +0300

    Fix bug with project structure

--- lib/fobos/graph_api/graph_api.rb
+++ lib/fobos/graph_api/graph_api.rb
@@ -1,7 +1,7 @@
 module Fobos
   module GraphAPI
-    require 'lib/fobos/graph_api/o_auth'
-    require 'lib/fobos/graph_api/users'
-    require 'lib/fobos/graph_api/pages'
+    require 'fobos/graph_api/o_auth'
+    require 'fobos/graph_api/users'
+    require 'fobos/graph_api/pages'
   end
 end
